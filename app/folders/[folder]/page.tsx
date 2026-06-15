14d9f225630dc372b16f0b552c7a73c7659f0fb2
Atikah <atikahnazir6@gmail.com>
2026-06-15 17:29:03 +0800

    [Feauture] Create route folders

--- app/folders/[folder]/page.tsx
+++ app/folders/[folder]/page.tsx
@@ -1,24 +1,21 @@
 import { Suspense } from "react";
+import Content from "./ContentPage";
 
 export default async function FolderPage({
   params,
 }: PageProps<"/folders/[folder]">) {
+  //const {folder} = await params;
   return (
     <div>
-      <h1>Folder name</h1>
+      {/* <h1>Folder name</h1> */}
       <Suspense fallback={<div>Loading...</div>}>
-        {params.then(({ folder }) => (
-          <Content folder={folder} />
-        ))}
+        {params.then(
+          ({ folder }) => (
+            console.log(folder, "folder name from params"),
+            (<Content folder={Number(folder)} />)
+          ),
+        )}
       </Suspense>
-    </div>
-  );
-}
-
-async function Content({ folder }: { folder: string }) {
-  return (
-    <div>
-      <h2>Folder: {folder}</h2>
     </div>
   );
 }
